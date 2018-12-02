06690a13c747f2ce51604cd181359ed1cc51b458
Egor Vorobyov <vorobyov_egor@bk.ru>
2018-12-02 22:37:00 +0300

    gradebook

---
+++ gradebook/student.h
@@ -0,0 +1,22 @@
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
+#include "person.h"
+#include "group.h"
+
+struct Student:Person
+{
+    Group group;
+    Student()=default;
+    Student(Person person,Group group_)
+    {
+        name = person.name;
+        surename = person.surename;
+        birthday = person.birthday;
+        group = group_;
+    }
+    void print2()
+    {
+        std::cout << name << " " << surename << " " <<std::endl;
+    }
+
+};
